513337e61e8d9c0586394a2d1a94fb917b1cc009
zeedie <coreyhollowayzee@gmail.com>
2025-07-23 17:32:21 +0530

    Merge remote and local

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import { storage } from "./storage";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -71,8 +73,8 @@ app.use((req, res, next) => {
   const port = 5000;
   server.listen({
     port,
-    host: "0.0.0.0",
-    reusePort: true,
+    host: "127.0.0.1",
+    // reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
   });
